1772522a44072f63af3c87a0a85cd1dbe4026227
Martin Wirth <33173906+MartinWirth@users.noreply.github.com>
2026-09-13 21:48:41 +0200

    Display datetime as ISO short without timezone

--- app/imap_client.py
+++ app/imap_client.py
@@ -108,7 +108,7 @@ class Mailbox:
         date = ""
         try:
             dt = parsedate_to_datetime(msg.get("Date", ""))
-            date = dt.strftime("%Y-%m-%d %H:%M")
+            date = dt.strftime("%Y-%m-%dT%H:%M")
         except (TypeError, ValueError, OverflowError):
             date = msg.get("Date", "")
 
